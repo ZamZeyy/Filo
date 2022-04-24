7ea4375e5d052e5929e1492f350ab2687314fc11
zamzeyy <mrjudka@gmail.com>
2022-04-25 00:44:36 +0100

    2nd

--- config/app.php
+++ config/app.php
@@ -125,6 +125,8 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
